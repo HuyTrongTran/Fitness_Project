22f0c055aa5c0aed1ea82eeebb4cfdbed460dae7
HuyTrongTran <tranhuytrong.1821@gmail.com>
2025-03-03 17:37:23 +0700

    app.js on BE

--- fitness_tracking/lib/screens/OnBoardingScreen/onboardingScreen.dart
+++ fitness_tracking/lib/screens/OnBoardingScreen/onboardingScreen.dart
@@ -1,6 +1,5 @@
 import 'package:fitness_tracking/constants/colors%20.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:http/http.dart' as http;
 
